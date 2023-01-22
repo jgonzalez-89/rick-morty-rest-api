62bff39cc5ef2cdab25144641ab28f17de93ea82
jgonzalez-89 <kbcerverjo@gmail.com>
2023-01-22 19:59:43 +0100

    Implement Redux pass props to modal.jsx

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "./button";
-
 import { ConnectedRickAndMorty } from './characters.jsx';
 
 export default function Modal() {
@@ -29,45 +28,16 @@ export default function Modal() {
                   {favorites.map((character) => (
                     <div key={character.id} className="p-4">
                       <div className="bg-white rounded-lg p-6">
-                        {/* <img
-                    src={character.image}
-                    alt={character.name}
-                    className="w-full mb-4 rounded-lg transform transition-all duration-200 hover:scale-110"
-                  /> */}
                         <h1 className="text-xl text-gray-800 font-medium mb-2">
                           {character.name}
                         </h1>
                         <p className="text-gray-600">
                           {character.gender} - {character.species}
                         </p>
-                        {/* <p
-                    className={
-                      character.status === "Alive"
-                        ? "text-green-700 ml-2"
-                        : character.status === "Dead"
-                        ? "text-red-700 ml-2"
-                        : "text-blue-700 ml-2"
-                    }
-                  >
-                    {character.status}
-                  </p> */}
-                        {/* <div
-                    className="relative inline-block text-lg group cursor-pointer"
-                    onClick={() => handleAddToFavorites(character)}
-                  >
-                  </div> */}
                       </div>
                     </div>
                   ))}
-                  {/* <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p> */}
                 </div>
-
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <Button onClick={() => setShowModal(false)} text="Close" />
